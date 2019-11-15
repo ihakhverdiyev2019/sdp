2c232ba9f88ff418d6063d215f1502572b98e578
ihakhverdiyev <ihakhverdiyev@mexxplus.com>
2019-11-15 19:49:50 +0400

    Updated

--- src/main/java/ada/spd/startup/Controllers/StartupperFind.java
+++ src/main/java/ada/spd/startup/Controllers/StartupperFind.java
@@ -2,9 +2,7 @@ package ada.spd.startup.Controllers;
 
 import ada.spd.startup.Domains.Startupper;
 import ada.spd.startup.Repositories.StartupperRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -17,13 +15,13 @@ public class StartupperFind {
         this.startupperRepository = startupperRepository;
     }
 
-    @PostMapping(
+    @GetMapping(
             value = "startupper/find", consumes = "application/json", produces = "application/json")
     public Optional<Startupper> findByID(@RequestBody Startupper startupper) {
         return startupperRepository.findById(startupper.getId());
 
     }
-    @PostMapping(
+    @GetMapping(
             value = "startupper/findall", consumes = "application/json", produces = "application/json")
     public Iterable <Startupper> findAll() {
         return startupperRepository.findAll();
